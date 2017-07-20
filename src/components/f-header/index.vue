d16e70d6338d2ab09fc4fc935672b77cf9606704
zc <776331229@qq.com>
2017-07-20 17:47:37 +0800

    导航栏ok

--- src/components/f-header/index.vue
+++ src/components/f-header/index.vue
@@ -9,18 +9,26 @@
           <!--导航菜单栏 start-->
           <ul class=" dis-inline-block float-left font-xi">
             <li>
-              <a href="/home.html" class="homeLink" title="首页">首页</a>
+              <a :class="{'homeLink' : pathName === '/home.html'}" href="/home.html" title="首页">首页</a>
             </li>
             <li>
-              <router-link to="/product" tag="a" title="产品介绍">产品介绍</router-link>
+              <a :class="{'homeLink' : pathName === '/product-introduce.html'}" href="/product-introduce.html" title="产品介绍">产品介绍</a>
             </li>
             <li>
-              <router-link to="/scanPay" tag="a" title="扫码支付">扫码支付</router-link>
+              <a :class="{'homeLink' : pathName === '/scan-pay.html'}" href="/scan-pay.html" title="扫码支付">扫码支付</a>
             </li>
-            <li><router-link to="/cooperation" tag="a" title="加盟合作">加盟合作</router-link></li>
-            <li><router-link to="/success-case" tag="a" title="成功案例">成功案例</router-link></li>
-            <li><router-link :to="{path:'/news',query:{type:0}}" tag="a" title="最新资讯">最新资讯</router-link></li>
-            <li><router-link to="/contact" tag="a" title="联系我们">联系我们</router-link></li>
+            <li>
+              <a  :class="{'homeLink' : pathName === '/cooperation.html'}" href="/cooperation.html" tag="a" title="加盟合作">加盟合作</a>
+            </li>
+            <li>
+              <a :class="{'homeLink' : pathName === '/successful-case.html'}" href="/successful-case.html" tag="a" title="成功案例">成功案例</a>
+            </li>
+            <li>
+              <a :class="{'homeLink' : pathName === '/news.html'}" href="news.html?type=0" tag="a" title="最新资讯">最新资讯</a>
+            </li>
+            <li>
+              <a :class="{'homeLink' : pathName === '/contact.html'}" href="/contact.html" tag="a" title="联系我们">联系我们</a>
+            </li>
             <li class="float-right" title="联系方式">联系方式 : <span class="font-color-blue">0571-88533296</span></li>
           </ul>
         </div>
@@ -32,8 +40,11 @@
   export default {
      data() {
        return {
-
+          pathName : '/home.html'
        }
+     },
+     created(){
+       this.pathName =  window.location.pathname;
      }
   }
 </script>
